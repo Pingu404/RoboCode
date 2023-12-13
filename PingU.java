544840d7e2db0f8dbb51495a733eb5af969806d7
Andrew <ajdwashburn@gmail.com>
2023-12-13 09:40:19 -0800

    Adding the inital robot file
    This is a bad inital robot. Basicly the defult, but much worse i added some strange spins to see what it can do.

---
+++ PingU.java
@@ -0,0 +1,60 @@
+package aw;
+import robocode.*;
+//import java.awt.Color;
+
+// API help : https://robocode.sourceforge.io/docs/robocode/robocode/Robot.html
+
+/**
+ * PingU - a robot by (your name here)
+ */
+public class PingU extends Robot
+{
+	/**
+	 * run: PingU's default behavior
+	 */
+	public void run() {
+		// Initialization of the robot should be put here
+
+		// After trying out your robot, try uncommenting the import at the top,
+		// and the next line:
+
+		// setColors(Color.red,Color.blue,Color.green); // body,gun,radar
+
+		// Robot main loop
+		while(true) {
+			// Replace the next 4 lines with any behavior you would like
+			fire(1);
+			ahead(100);
+			turnGunRight(360);
+			back(100);
+			turnGunLeft(360);
+		}
+	}
+ 
+	
+	/**m
+	 * onScannedRobot: What to do when you see another robot
+	 */
+	public void onScannedRobot(ScannedRobotEvent e) {
+		// Replace the next line with any behavior you would like
+		fire(1);
+		attack();
+		
+	}
+
+	/**
+	 * onHitByBullet: What to do when you're hit by a bullet
+	 */
+	public void onHitByBullet(HitByBulletEvent e) {
+		// Replace the next line with any behavior you would like
+		back(10);
+	}
+	
+	/**
+	 * onHitWall: What to do when you hit a wall
+	 */
+	public void onHitWall(HitWallEvent e) {
+		// Replace the next line with any behavior you would like
+		back(20);
+	}	
+}
